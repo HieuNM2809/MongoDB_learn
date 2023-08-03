1119dfe572429b0a7de56729f29a218b07c282f4
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-08-03 17:08:44 +0700

    7_Indexing & Search.sql và 8_validate.sql

---
+++ 7_Indexing & Search.sql
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  {
+    $search: {
+      index: "default", // optional unless you named your index something other than "default"
+      text: {
+        query: "star wars",
+        path: "title"
+      },
+    },
+  },
+  {
+    $project: {
+      title: 1,
+      year: 1,
+    }
+  }
+]
---
+++ 8_validate.sql
@@ -0,0 +1,34 @@
+db.createCollection("posts", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: [ "title", "body" ],
+      properties: {
+        title: {
+          bsonType: "string",
+          description: "Title of post - Required."
+        },
+        body: {
+          bsonType: "string",
+          description: "Body of post - Required."
+        },
+        category: {
+          bsonType: "string",
+          description: "Category of post - Optional."
+        },
+        likes: {
+          bsonType: "int",
+          description: "Post like count. Must be an integer - Optional."
+        },
+        tags: {
+          bsonType: ["string"],
+          description: "Must be an array of strings - Optional."
+        },
+        date: {
+          bsonType: "date",
+          description: "Must be a date - Optional."
+        }
+      }
+    }
+  }
+})
