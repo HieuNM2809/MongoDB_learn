102cfcef1e6103e750574b35d031919d580cd61b
HieuNM <nmhieucoder@gmail.com>
2023-08-20 20:36:31 +0700

    Update 3.2_query operators_code.sql

--- 3.2_query operators_code.sql
+++ 3.2_query operators_code.sql
@@ -13,4 +13,21 @@ db.movies.find({
     { title: "superman" },
     { title1: "superman1" },
 })
-SELECT * FROM movies WHERE title = "superman" OR title = "superman1";
+=> SELECT * FROM movies WHERE title = "superman" OR title = "superman1";
+
+--------------------------------------------------------------------------------
+
+// Tìm tất cả các tài liệu có trường "age"
+db.collection.find({ age: { $exists: true } })
+// Tìm tất cả các tài liệu có trường "age" tồn tại và không phải là null
+db.collection.find({ age: { $exists: true, $ne: null } })
+
+--------------------------------------------------------------------------------
+// Find documents where sinh > toan
+db.students.find({ sinh: { $gt: "$toan" } })
+
+db.students.find({
+  $expr: {
+    $gt: ["$sinh", "$toan"]
+  }
+})
